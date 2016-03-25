3a64677c6dc2879b47e7a63729225027467c181f
birm <birm@gatech.edu>
2016-03-25 17:23:43 -0400

    debug search 5 other wildcards, hope not escaped

--- app/controllers/SearchController.php
+++ app/controllers/SearchController.php
@@ -13,11 +13,11 @@ class SearchController extends BaseController
     }
     public function find()
     {
-        $search = Input::get('email');
+        $search = '*'.Input::get('email').'*';
         $state = Input::get('state');
         //soon, add a fulltext field to optimize this, 
         // step 1 (sql) ALTER TABLE bills ADD FULLTEXT ft_index_name(title, id,description,text);
-        $results = DB::select('SELECT * FROM bills WHERE (match (title, id,description,text) against *(:search)*) AND (state =(:state))',['search'=>$search,'state'=>$state]);
+        $results = DB::select('SELECT * FROM bills WHERE (match (title, id,description,text) against (:search)) AND (state =(:state))',['search'=>$search,'state'=>$state]);
         return View::make('search')->with('results',$results);
 
         return View::make('search');
