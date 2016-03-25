7914b358c8b2db1ae50eba73a4eb9efe634ec218
birm <birm@gatech.edu>
2016-03-25 17:33:38 -0400

    debug search 5 other wildcards, hope not escaped

--- app/controllers/SearchController.php
+++ app/controllers/SearchController.php
@@ -17,7 +17,7 @@ class SearchController extends BaseController
         $state = Input::get('state');
         //soon, add a fulltext field to optimize this, 
         // step 1 (sql) ALTER TABLE bills ADD FULLTEXT ft_index_name(title, id,description,text);
-        $results = DB::select('SELECT * FROM bills WHERE (match (title, id,description,text) against (:search)) AND (state =(:state))',['search'=>$search,'state'=>$state]);
+        $results = DB::select('SELECT * FROM bills WHERE (match (title,id,description,text) against (:search IN BOOLEAN MODE)) AND (state like (:state))',['search'=>$search,'state'=>$state]);
         return View::make('search')->with('results',$results);
 
         return View::make('search');
