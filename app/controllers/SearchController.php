e41ef2d5b1c0c2cd31c5e5671844580c5257284b
birm <birm@gatech.edu>
2016-03-25 19:25:51 -0400

    debug search 5 other wildcards, hope not escaped

--- app/controllers/SearchController.php
+++ app/controllers/SearchController.php
@@ -16,7 +16,7 @@ class SearchController extends BaseController
         $search = Input::get('search');
         $state = Input::get('state');
         //$results = DB::select('SELECT * FROM fullbill WHERE ((match (title,id,description,text) against (:search)) AND (state like (:state)))',['search'=>$search,'state'=>$state]);
-        $results = DB::select('SELECT * FROM fullbill WHERE (title like (:search))',['search'=>$search]);
+        $results = DB::select('SELECT * FROM fullbill WHERE title like :search',['search'=>$search]);
         return View::make('search')->with('results',$results);
     }
 }
