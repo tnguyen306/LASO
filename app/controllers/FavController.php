f5d97fe5282d16868335c4b2da6d530178eb6669
birm <birm@gatech.edu>
2016-04-01 04:08:35 -0400

    add legislator name to fav screen

--- app/controllers/FavController.php
+++ app/controllers/FavController.php
@@ -8,22 +8,23 @@ class FavController extends BaseController {
         foreach ($favorites as $favorite){
             //parse
             $t_type=$favorite->type;
-            $t_item="%".trim($favorite->item,' ')."%";
+            $t_item=trim($favorite->item,' ');
+            $t_items="%".trim($favorite->item,' ')."%";
             $t_id=$favorite->id;
             //search accordingly
             if ($t_type=="bill"){
-                $t_res=DB::select('select * from fullbill where id like :search order by updated_at DESC limit 5',['search'=>$t_item]);
+                $t_res=DB::select('select * from fullbill where id like :search order by updated_at DESC limit 5',['search'=>$t_items]);
             }elseif ($t_type=="legislator"){
-                $t_res=DB::select('select * from fullbill where (a_id like ? or b_id like ?) order by updated_at DESC limit 5',[$t_item,$t_item]);
-                $legislator = DB::select('Select first_name,last_name,district,bio,id,photo_path from legislators where id=?',[$id]);
+                $t_res=DB::select('select * from fullbill where (a_id like ? or b_id like ?) order by updated_at DESC limit 5',[$t_items,$t_items]);
+                $legislator = DB::select('Select first_name,last_name,district,bio,id,photo_path from legislators where id=?',[$t_item]);
                 foreach ($legislator as $leg) {
                 }
                 $t_item=$leg->first_name.$leg->last_name;
             } else { //keyword or similar
-                $t_res=DB::select('SELECT * FROM fullbill WHERE ((id like :search1) or (description like :search2) or (title like :search3)) order by updated_at DESC limit 10',['search1'=>$t_item,'search2'=>$t_item,'search3'=>$t_item]);
+                $t_res=DB::select('SELECT * FROM fullbill WHERE ((id like :search1) or (description like :search2) or (title like :search3)) order by updated_at DESC limit 10',['search1'=>$t_items,'search2'=>$t_items,'search3'=>$t_items]);
             }
             //new array for this iteration
-            $t_arr=array("id"=>$t_id,"type"=>$t_type,"item"=>$favorite->item,"result"=>$t_res);
+            $t_arr=array("id"=>$t_id,"type"=>$t_type,"item"=>$t_item,"result"=>$t_res);
             //now update the master array
             array_push($results,$t_arr);
         }
