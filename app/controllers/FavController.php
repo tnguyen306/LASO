aad16f41c1797c92e77d4631501f05fc7c2fcccf
birm <birm@gatech.edu>
2016-04-01 02:02:16 -0400

    quick favorite fix

--- app/controllers/FavController.php
+++ app/controllers/FavController.php
@@ -11,9 +11,9 @@ class FavController extends BaseController {
             $t_id=$favorite->id;
             //search accordingly
             if ($t_type=="bill"){
-                $t_res=DB::select('select * from fullbill where id like :search % order by updated_at DESC limit 5',['search'=>$t_item]);
+                $t_res=DB::select('select * from fullbill where id like :search order by updated_at DESC limit 5',['search'=>$t_item]);
             } elseif ($t_type=="legislator"){
-                $t_res=DB::select('select * from fullbill where (a_id like "?" or b_id like "?") order by updated_at DESC limit 5',[$t_item,$t_item]);
+                $t_res=DB::select('select * from fullbill where (a_id like ? or b_id like ?) order by updated_at DESC limit 5',[$t_item,$t_item]);
             } else { //keyword or similar
                 $t_res=DB::select('SELECT * FROM fullbill WHERE ((id like :search1) or (description like :search2) or (title like :search3)) order by updated_at DESC limit 10',['search1'=>$t_item,'search2'=>$t_item,'search3'=>$t_item]);
             }
