089461e8a5129d0f6097c065b5d103c40aa5a849
birm <birm@gatech.edu>
2016-04-05 18:54:42 -0400

    fix compare search, 1

--- app/controllers/CompareController.php
+++ app/controllers/CompareController.php
@@ -18,7 +18,7 @@ class CompareController extends BaseController
         $search = '%'.Input::get('search').'%';
         $state = Input::get('state');
         $limit = Input::get('limit');
-        $results = DB::select('SELECT * FROM bills WHERE ((id like :search1) or (description like :search2) or (title like :search3)) AND (state = (:state)) limit :limit',['search1'=>$search,'search2'=>$search,'search3'=>$search,'state'=>$state,'limit'=>$limit]);
+        $results=DB::select('SELECT * FROM bills WHERE ((id like :search1) or (description like :search2) or (title like :search3)) AND (state like (:state)) limit :limit',['search1'=>$search,'search2'=>$search,'search3'=>$search,'state'=>$state,'limit'=>$limit]);
         return View::make('selectcompare')->with(array('results'=>$results,'parent'=>$id));
     }
 }
