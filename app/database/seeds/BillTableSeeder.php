41ecdfe86ae28a5dcee16a4cf5b8667ad2b2721b
birm <birm@gatech.edu>
2016-04-05 00:43:09 -0400

    speed seeding, try 1

--- app/database/seeds/BillTableSeeder.php
+++ app/database/seeds/BillTableSeeder.php
@@ -22,104 +22,97 @@ class BillTableSeeder extends Seeder {
 
         for ($x = 0; $x < count($ids); $x++) {
             $urladd = str_replace(' ','%20',$ids[$x]);
+            $session = str_replace(' ','%20',$session);
             echo $state.$urladd . "\n";
 	        try{$json2 = file_get_contents('http://openstates.org/api/v1//bills/'.$state.'/'.$session.'/'.$urladd.'/?apikey=e2f56937c8c74a67a0f6133152f0c2f2');
-            $js2 = json_decode($json2);
-            $jsonIterator2 = new RecursiveIteratorIterator(
-                new RecursiveArrayIterator(json_decode($json2, TRUE)),
-                RecursiveIteratorIterator::SELF_FIRST);
-            foreach ($jsonIterator2 as $k2 => $v2) {
-	            if(strcmp($k2,"title")==0){
-		            $ititle=$v2;
-	        }elseif(strcmp($k2,"description")==0){
-		            $idesc=$v2;
-	        }elseif(strcmp($k2,"state")==0){
-		            $istate=$v2;
-	        }elseif(strcmp($k2,"sponsors")==0){
-                    try{
-                        $iauthor=$v2[0]['leg_id'];
-                        $icoauthor=$v2[1]['leg_id'];
-                    }catch(Exception $e){
-                        $pass_var=1;
-                    }
-	        }elseif(strcmp($k2,"updated_at")==0){
-		            $iupdated=$v2;
-	        }elseif(strcmp($k2,"created_at")==0){
-		            $icreated=$v2;
-	        }elseif(strcmp($k2,"versions")==0){
-                    $r=0; // revision number                   
-                    foreach ($v2 as $w2) {
-                        
-                        // set unset variables
-                        if(isset($idesc)){
-                        $idesc=$idesc;
+            //$js2 = json_decode($json2);
+           // $jsonIterator2 = new RecursiveIteratorIterator(
+                //new RecursiveArrayIterator(json_decode($json2, TRUE)),
+                //RecursiveIteratorIterator::SELF_FIRST);
+            $jsonIterator2=json_decode($json2);
+            $ititle=$jsonIterator2['title'];
+	        $idesc=$jsonIterator2['description'];
+	        $istate=$jsonIterator2['state'];
+	        $v2=$jsonIterator2['sponsors'];
+            try{
+                $iauthor=$v2[0]['leg_id'];
+                $icoauthor=$v2[1]['leg_id'];
+            }catch(Exception $e){
+                $pass_var=1;
+            }
+		    $iupdated=$jsonIterator2['updated_at'];
+		    $icreated=$jsonIterator2['created_at'];
+	        $v2=$jsonIterator2['versions'];
+            $r=0; // revision number                   
+            foreach ($v2 as $w2) {
+                
+                // set unset variables
+                if(isset($idesc)){
+                $idesc=$idesc;
+                }else{
+                $idesc="No Description Found";
+                }
+                if(isset($iauthor)){
+                $iauthor=$iauthor;
+                }else{
+                $iauthor="0";
+                }
+                if(isset($icoauthor)){
+                $icoauthor=$icoauthor;
+                }else{
+                $icoauthor="1";
+                }
+                // manage revisions
+                $r=$r+1;            
+                $iurl = $w2['url']; // url
+                $mimetype = $w2['mimetype'];
+                //different states have different methods for full text
+                $itxt="Error in Fetch; try path for now";
+                try{
+                        if(($mimetype=="application/pdf")or (substr($w2['url'],-3,3)=='pdf') or (substr($w2['url'],-3,3)=='PDF')){
+                            $itxt=pdf2text($w2['url']); // pdf text
                         }else{
-                        $idesc="No Description Found";
+                            //html rules
+                            if($istate=='fl'){
+                                $dom = new domDocument('1.0', 'utf-8');
+                                $ihtml =file_get_contents($w2['url']);
+                                $dom->loadHTML($ihtml);
+                                $pre= $dom->getElementsByTagName('pre');
+                                $itxt = $pre->item(0)->nodeValue;
+                            }elseif($istate=='ca'){
+                                $dom = new domDocument('1.0', 'utf-8');
+                                $ihtml =file_get_contents($w2['url']);
+                                $dom->loadHTML($ihtml);
+                                $pre= $dom->getElementById('bill_all');
+                                $itxt = $pre->item(0)->nodeValue;
+                            }else{
+                                $dom = new domDocument('1.0', 'utf-8');
+                                $ihtml =file_get_contents($w2['url']);
+                                $dom->loadHTML($ihtml);
+                                $pre= $dom->getElementsByTagName('body');
+                                $itxt = $pre->item(0)->nodeValue;
+                            }
                         }
-                        if(isset($iauthor)){
-                        $iauthor=$iauthor;
-                        }else{
-                        $iauthor="0";
-                        }
-                        if(isset($icoauthor)){
-                        $icoauthor=$icoauthor;
-                        }else{
-                        $icoauthor="1";
-                        }
-                        // manage revisions
-                        $r=$r+1;            
-                        $iurl = $w2['url']; // url
-                        $mimetype = $w2['mimetype'];
-                        //different states have different methods for full text
-                        $itxt="Error in Fetch; try path for now";
-                        try{
-                                if(($mimetype=="application/pdf")or (substr($w2['url'],-3,3)=='pdf') or (substr($w2['url'],-3,3)=='PDF')){
-                                    $itxt=pdf2text($w2['url']); // pdf text
-                                }else{
-                                    //html rules
-                                    if($istate=='fl'){
-                                        $dom = new domDocument('1.0', 'utf-8');
-                                        $ihtml =file_get_contents($w2['url']);
-                                        $dom->loadHTML($ihtml);
-                                        $pre= $dom->getElementsByTagName('pre');
-                                        $itxt = $pre->item(0)->nodeValue;
-                                    }elseif($istate=='ca'){
-                                        $dom = new domDocument('1.0', 'utf-8');
-                                        $ihtml =file_get_contents($w2['url']);
-                                        $dom->loadHTML($ihtml);
-                                        $pre= $dom->getElementById('bill_all');
-                                        $itxt = $pre->item(0)->nodeValue;
-                                    }else{
-                                        $dom = new domDocument('1.0', 'utf-8');
-                                        $ihtml =file_get_contents($w2['url']);
-                                        $dom->loadHTML($ihtml);
-                                        $pre= $dom->getElementsByTagName('body');
-                                        $itxt = $pre->item(0)->nodeValue;
-                                    }
-                                }
-                            // push this revision to the database
-                            $new_derived_key = $istate.$ids[$x].'r'.strval($r);
-                            $newbill = new Bill;
-                            $newbill->id =$new_derived_key;
-                            $newbill->state=$istate;
-                            $newbill->ext_id =$istate.$ids[$x].' rev'.strval($r);
-                            $newbill->title=$ititle;
-                            $newbill->text =$itxt;
-                            $newbill->amount="coming";
-                            $newbill->status ="status";
-                            $newbill->introduced_date=$icreated;
-                            $newbill->passed_date =$iupdated;
-                            $newbill->revision_id=strval($r);
-                            $newbill->doc_path =$iurl;
-                            $newbill->author_id=$iauthor;
-                            $newbill->coauthor_id =$icoauthor;
-                            $newbill->save();
-                        }catch(Exception $e){
-                        $pass_var=1;
-                        }
-                    }
-	        }
-
+                    // push this revision to the database
+                    $new_derived_key = $istate.$ids[$x].'r'.strval($r);
+                    $newbill = new Bill;
+                    $newbill->id =$new_derived_key;
+                    $newbill->state=$istate;
+                    $newbill->ext_id =$istate.$ids[$x].' rev'.strval($r);
+                    $newbill->title=$ititle;
+                    $newbill->text =$itxt;
+                    $newbill->amount="coming";
+                    $newbill->status ="status";
+                    $newbill->introduced_date=$icreated;
+                    $newbill->passed_date =$iupdated;
+                    $newbill->revision_id=strval($r);
+                    $newbill->doc_path =$iurl;
+                    $newbill->author_id=$iauthor;
+                    $newbill->coauthor_id =$icoauthor;
+                    $newbill->save();
+                }catch(Exception $e){
+                $pass_var=1;
+                }
         }
     }catch(Exception $e){
         print $e;
@@ -135,10 +128,10 @@ class BillTableSeeder extends Seeder {
         //self::run_state('tx','84'); // no bills, somehow
         //self::run_state('tn','109');
         //self::run_state('ma','189th'); 
-        self::run_state('me','127'); //sometimes rtf? no good support yet
+        //self::run_state('me','127'); //sometimes rtf? no good support yet
         //self::run_state('ca','20152016');
         self::run_state('or','2016 Regular Session'); // this didn't work previously, why?
-        self::run_state('wa','2015-2016');
+        //self::run_state('wa','2015-2016'); // pdf AND html
         
 
     }
