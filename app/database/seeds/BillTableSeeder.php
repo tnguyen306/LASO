6a0304c9f729537636a533ac79fcf9a8cf98d51a
birm <birm@gatech.edu>
2016-04-05 04:41:49 -0400

    add new OR parser, try 1

--- app/database/seeds/BillTableSeeder.php
+++ app/database/seeds/BillTableSeeder.php
@@ -1,7 +1,21 @@
 <?php
 require_once('pdf_to_text.php');
 class BillTableSeeder extends Seeder {
-
+    public function ExtractTextFromPdf ($pdfdata) {
+        if (strlen ($pdfdata) < 1000 && file_exists ($pdfdata)) $pdfdata = file_get_contents ($pdfdata); //get the data from file
+        if (!trim ($pdfdata)) echo "Error: there is no PDF data or file to process.";
+        $result = ''; //this will store the results
+        //Find all the streams in FlateDecode format (not sure what this is), and then loop through each of them
+        if (preg_match_all ('/<<[^>]*FlateDecode[^>]*>>\s*stream(.+)endstream/Uis', $pdfdata, $m)) foreach ($m[1] as $chunk) {
+	        $chunk = gzuncompress (ltrim ($chunk)); //uncompress the data using the PHP gzuncompress function
+	        $chunk = iconv ('UTF-8', 'ASCII//TRANSLIT', $chunk); //suggested in comments to code above to remove junk characters
+	        //If there are [] in the data, then extract all stuff within (), or just extract () from the data directly
+	        $a = preg_match_all ('/\[([^\]]+)\]/', $chunk, $m2) ? $m2[1] : array ($chunk); //get all the stuff within []
+	        foreach ($a as $subchunk) if (preg_match_all ('/\(([^\)]+)\)/', $subchunk, $m3)) $result .= join ('', $m3[1]); //within ()
+        }
+        else echo "Error: there is no FlateDecode text in this PDF file that I can process.";
+        return $result; //return what was found
+    }
     public function run_state($state,$session)
     {
         $json = file_get_contents('http://openstates.org/api/v1//bills/?state='.$state.'&last_action_since=2015-10-10&apikey=e2f56937c8c74a67a0f6133152f0c2f2');
@@ -84,7 +98,11 @@ class BillTableSeeder extends Seeder {
                                 $itxt=str_replace("</body>"," ",$itxt);
                                 $itxt=str_replace("</html>"," ",$itxt);
                             }elseif(strcmp($istate,'or')==0){
-                                $itxt=pdf2text($w2['url']); // pdf text
+                                //$itxt=pdf2text($w2['url']); // pdf text
+                                echo ("or method: ");
+                                echo $w2['url']. "\n";
+                                $pdfdata=file_get_contents($w2['url']);
+                                $itxt=self::ExtractTextFromPdf($pdfdata);
                             }else{
                                 echo ("general method: ");
                                 echo $w2['url']. "\n";
