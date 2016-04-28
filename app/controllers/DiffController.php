4594fb212ae7dc3d2f8d8b75c4e428466e66d867
birm <birm@gatech.edu>
2016-04-28 15:28:07 -0400

    try memory fix for intensive operations

--- app/controllers/DiffController.php
+++ app/controllers/DiffController.php
@@ -4,11 +4,9 @@ class DiffController extends BaseController {
     public function diff($i1,$i2)
     {
      ini_set('memory_limit', '256M');
-     $d1 = DB::select('Select * from bills WHERE id=:id limit 1',['id' => $i1])->first();
-     $d2 = DB::select('Select * FROM bills WHERE id=:id limit 1',['id' => $i2]);
-      $a11=DB::select('Select first_name,last_name,district,bio,id,photo_path from legislators where id=?',[$d1->author_id]);
-foreach ($a11 as $a1) {
-}
+     $d1 = DB::select('Select * from bills WHERE id=:id limit 1',['id' => $i1])[0]
+     $d2 = DB::select('Select * FROM bills WHERE id=:id limit 1',['id' => $i2])->first();
+      $a1=DB::select('Select first_name,last_name,district,bio,id,photo_path from legislators where id=?',[$d1->author_id])->first();
       $c1=DB::select('Select first_name,last_name,district,bio,id,photo_path from legislators where id=?',[$d1->coauthor_id])->first();
       $an1=$a1->first_name." ".$a1->last_name;
       $cn1=$c1->first_name." ".$c1->last_name;
