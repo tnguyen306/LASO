3cae1d4f8aea07c292310a79484b45298be98947
Ryan Birmingham <birm@gatech.edu>
2016-06-03 02:58:40 -0400

    fixed ; missings

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -5,37 +5,37 @@ class ApiController extends BaseController
     // no security at all, need to work on api keys or user ids
     public function docs()
     {
-        $docs = DB::table('docs')->get();
-        return json_encode($results)
+        $docs = DB::select('Select * from docs');
+        return json_encode($results);
     }
     
     public function doc($id)
     {
         $docs = DB::table('docs')->where('id', $id)->get();
-        return json_encode($results)
+        return json_encode($results);
     }
 
     public function bills()
     {
         $results = DB::select('Select * from bills');
-        return json_encode($results)
+        return json_encode($results);
     }
 
     public function bill($id)
     {
         $docs = DB::table('bills')->where('id', $id)->get();
-        return json_encode($results)
+        return json_encode($results);
     }
 
     public function legislators()
     {
         $results = DB::select('Select * from legislators');
-        return json_encode($results)
+        return json_encode($results);
     }
 
     public function legislator($id)
     {
         $docs = DB::table('legislators')->where('id', $id)->get();
-        return json_encode($results)
+        return json_encode($results);
     }
 }
