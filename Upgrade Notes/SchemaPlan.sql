62d8c042275fef6182c9adb9b9e15a7fc0a31ffe
Ryan Birmingham <birm@rbirm.us>
2017-03-08 13:58:46 -0500

    added basic new schema plan

---
+++ Upgrade Notes/SchemaPlan.sql
@@ -0,0 +1,29 @@
+/*User*/
+Login
+Email
+Password Hash
+Key
+/*Bill*/
+Session
+Title
+Prefix
+Number
+Link
+Text
+Author IDs
+/*State*/
+Name
+Abbreviation
+API URL
+API Method
+Options
+/*Legislator*/
+Name
+Photo URL
+ID
+District
+/*Favorite*/
+Owner
+Type
+ID
+Weight(?)
