33b379eb6031f34fec73c8a9cacc72ebc6337cda
Ryan Birmingham <birm@rbirm.us>
2017-03-14 15:34:32 -0400

    changed fetch schema

--- Fetch/schema.sql
+++ Fetch/schema.sql
@@ -8,20 +8,17 @@ CREATE TABLE errors
 
 /*queue*/CREATE TABLE queue
              (
-                          `priority` INT DEFAULT 0,
-                          `state`    CHAR(2) NOT NULL,
-                          `min`      VARCHAR(64),
-                          `max`      VARCHAR(64),
-                          PRIMARY KEY (state, min, max)
+                          `priority`    INT DEFAULT 0,
+                          `state`       CHAR(2) NOT NULL,
+                          `min`         VARCHAR(64),
+                          `max`         VARCHAR(64),
+                          `in_progress` CHAR(1) PRIMARY KEY (state, min, max)
              );
 
 /*FetchLog*/CREATE TABLE fetchlog
-            (
-                         `id`           INT auto_increment NOT NULL,
-                         `recordsadded` INT NOT NULL DEFAULT 0,
-                         `state`        CHAR ( 2 ) NOT NULL,
-                         `timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                         `detail` TEXT,
-                         FOREIGN KEY `state` references state ( abbreviation ) ,
-                         PRIMARY KEY ( id )
-            );
+             (
+                          `id`           INT auto_increment NOT NULL,
+                          `recordsadded` INT NOT NULL DEFAULT 0,
+                          `timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                          PRIMARY KEY ( id )
+             );
