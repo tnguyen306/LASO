83efdef01ac5f2f57f11bd1f0d6cc6e391b48fb0
Ryan Birmingham <birm@rbirm.us>
2017-03-14 16:00:25 -0400

    documentation and method chnge

--- Fetch/methods/base.py
+++ Fetch/methods/base.py
@@ -2,38 +2,73 @@ import json
 from contextlib import contextmanager
 
 class BaseMethod(Object):
-    # Construct; Handle default and specific invocations
+    """
+    Base tools to fetch and use bills and other objects from an API and process them gracefully with a generator.
+    Defines a base api usage method; inhereting classes will likely have a different api() function.
+    """
+
+
     def __init__(self, *args, **kwargs):
+        """Handle general initialization for all classes."""
+        self.type = type
         self.args = args
         self.kwarfs = kwargs
-        self.done= []
+        # get next thing in queue
+        # select * from queue where in_progress<>'Y' order by priority desc;
+        # set those variables
+        self.maximum = ""
+        self.minimum = ""
+        self.state = ""
+        self.queue_id = ""
+        # update the queue to put that in progress
+        self.processed = 0
+        # keep track of what to put in min if we don't finish
+        self.last = self.minimum
 
-    # handle errors and
-    @contextmanager
-    def work(self, start, end):
-        # maintain database solution
-        # update on open
-        try:
-            yield
-        except Exception as err:
-            pass
-            # insert e information to error
-        # update from close
+    def __iter__(self):
+        """
+        Return the iterator/generator object.
+        """
+        return self
 
-    # get api information from table
+    def __next__(self):
+        """
+        Get the next item in the generator.
+        """
+        return self.next()
+
+    # search the api for the next item
     def api(self):
+        """
+        Search the api for the next item.
+        """
         pass
 
-    # get item generator
-    def items(self):
-        item = []
-        yield item
+    def next(self):
+        """
+        Get the next item in the generator.
+        """
+        # grab a thing to work on
+        # work on it
+        try:
+            item = []
+            yield item
+        except Exception as err:
+            # log the exception
+            raise err
+        # update internal log
+        # end generator when at end
 
-    # put in table
-    def put(self):
-        pass
-
-    # handle errors
-    de
-
-    # return indication of what succeeded
+    def __del__(self):
+        """
+        Handle queue and log before deletion.
+        """
+        # log
+        if True:
+            pass
+            # if we finished it
+            # delete from queue where id = {id} self.queue_id
+        else:
+            pass
+            # if we did not
+            # update queue set min = {min} , in_progress='N'; self.last
